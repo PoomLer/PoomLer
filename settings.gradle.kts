c86a06e35bea2e38140b023cc179a238985c4ee4
thawzintoe <developer.ptut@gmail.com>
2023-03-16 16:57:56 +0700

    Add all core modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,12 +5,30 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        // Register the AndroidX snapshot repository first so snapshots don't attempt (and fail)
+        // to download from the non-snapshot repositories
+        maven(url = "https://androidx.dev/snapshots/builds/8455591/artifacts/repository") {
+            content {
+                // The AndroidX snapshot repository will only have androidx artifacts, don't
+                // bother trying to find other ones
+                includeGroupByRegex("androidx\\..*")
+            }
+        }
         google()
         mavenCentral()
     }
 }
+
 rootProject.name = "PoomLer"
 include(":app")
+include(":build-logic")
+include(":core-data")
+include(":core-common")
+include(":core-network")
+include(":core-database")
+include(":core-model")
+include(":core-ui")
